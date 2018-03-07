0ca310f0f8bef8bace6056baf7d47cea2edd75e0
vangx033 <seeho.vang@hotmail.com>
2018-03-07 05:01:52 +0000

    Week8Script.sh

--- Week8Script.sh
+++ Week8Script.sh
@@ -31,3 +31,38 @@ udevadm info --query=all --name=/dev/sda
 
 udevadm monitor
 
+# chapter 4 
+
+# View current mounted filesystem status
+mount | less
+
+sudo dmesg | less
+
+# View Universally Unique Identify (UUID)
+sudo blkid
+
+# View the size and utilization of the current mounted filesystems
+df
+
+free
+
+
+# Create a hardlink point dir_2/file5 to dir_1/file2
+sudo mkdir dir_1
+sudo mkdir dir_2
+
+sudo echo a > dir_1/file_1.txt
+sudo echo b > dir_1/file_2.txt
+sudo echo b > dir_1/file_3.txt
+sudo echo d > dir_2/file_5.txt
+sudo ln dir_1/file_2.txt dir_2/file_5.txt
+
+# To verify inode
+ls -i
+
+# View kernel's boot and messages
+sudo dmesg | less
+
+# Go back to home directory and view kernel's parameters
+cd ..
+cat /proc/cdmline
