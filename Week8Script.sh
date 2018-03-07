c25faec80e2b1257516dda4a5038528c69629d4b
vangx033 <seeho.vang@hotmail.com>
2018-03-07 04:27:32 +0000

    Week8Script.sh

--- Week8Script.sh
+++ Week8Script.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 #Xee Ho Vang
 #Week 8 script
+
+echo "Hello Class"
+
+mkdir dir1
+
+cd dir1
+
+touch file1.txt file2.txt
+echo "This is testing" > file1.txt
+
+cat file1.txt
+
+rm file2.txt
+
+chmod 755 file1.txt
+
